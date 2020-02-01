47719bbb2b6833f4253ed5fd8a78e48e669d9518
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2020-02-01 09:57:04 +0100

    +mouvement

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "structures.h"
 #include "main.h"
 
 
-void initBoard(state board[7][7]) {
+
+void initBoard(state* board[7][7]) {
     //Initialise the board to an English solitaire
     for (int i=0; i<7; i++) {
         for (int j=0; j<7; j++) {
             if ( (i%6==0 && j%6==0) || (i%6==0 && j==1) || (i==1 && j%6==0) || (i%6==0 && j==5) || (i==5 && j%6==0) ) {
-                board[i][j] = out;
+                *board[i][j] = out;
             }
             else if ( i==j && i==3 ) {
-                board[i][j] = empty;
+                *board[i][j] = empty;
             }
             else {
-                board[i][j] = ball;
+                *board[i][j] = ball;
             }
         }
     }
 }
 
-int ball_nb(state board[7][7]) {
+int ballNb(state* board[7][7]) {
     //Counts the number of balls on the board
     int count =0;
     for (int i=0; i<7; i++) {
         for (int j=0 ; j<7 ; j++) {
-            if (board[i][j]==ball) {
+            if (*board[i][j]==ball) {
                 count++;
             }
         }
@@ -33,11 +36,39 @@ int ball_nb(state board[7][7]) {
     return count;
 }
 
-//int check_move
-int possible_move(state board[7][7]) {
+int correctMove(state* board[7][7], movement* move) {
+    int ok;
+    //Check if the position is ball and if the next positions are empty
+    int x = move->posix;
+    int y = move->posiy;
+    direction dir = move->dir;
+    if (board[x][y]==ball){
+        if ((dir==north) && (*board[x][y+1]==ball) && (*board[x][y+2]==empty)){
+            ok = 1;
+        }
+        else if ((dir==south) && (*board[x][y-1]==ball) && (*board[x][y-2]==empty)){
+            ok = 1;
+        }
+        else if ((dir==east) && (*board[x+1][y]==ball) && (*board[x+2][y]==empty)){
+            ok = 1;
+        }
+        else if ((dir==west) && (*board[x-1][y]==ball) && (*board[x-2][y]==empty)){
+            ok = 1;
+        }
+        else {
+            ok = 0;
+        }
+    }
+    else {
+        ok = 0;
+    }
+    return ok;
+}
+
+int possibleMove(state* board[7][7]) {
     //Returns 1 if one move can still be done
     int doable;
-    if (ball_nb(board) >= 2){
+    if (ballNb(board) >= 2){
         //If there is only one ball, one can not play anymore
         doable = 1;
     }
@@ -47,12 +78,79 @@ int possible_move(state board[7][7]) {
     return doable;
 }   
 
-void user_move(state board[7][7]) {
-    printf("Entrez les coordonnées du point que vous souhaitez bouger :\n");
+void doMove(state* board[7][7], movement* move) {
+    //Makes the move
+    //You must check that the move is correct before calling this function !
+    int x = move->posix;
+    int y = move->posiy;
+    if ((move->dir==north) && (*board[x][y+1]==ball) && (*board[x][y+2]==empty)){
+        *board[x][y] = empty;
+        *board[x][y+1] = empty;
+        *board[x][y+2] = empty;
+    }
+    else if ((move->dir==south) && (*board[x][y-1]==ball) && (*board[x][y-2]==empty)){
+        *board[x][y] = empty;
+        *board[x][y-1] = empty;
+        *board[x][y-2] = ball;
+    }
+    else if ((move->dir==east) && (*board[x+1][y]==ball) && (*board[x+2][y]==empty)){
+        *board[x][y] = empty;
+        *board[x+1][y] = empty;
+        *board[x+2][y] = ball;
+    }
+    else if ((move->dir==west) && (*board[x-1][y]==ball) && (*board[x-2][y]==empty)){
+        *board[x][y] = empty;
+        *board[x-1][y] = empty;
+        *board[x-2][y] = ball;
+    }
+    else {
+        printf("UNEXPECTED ERROR");
+    }
+}
 
-    printf("Entrez la direction du mouvement :\n");
+void userMove(state* board[7][7]) {
+    //Allows the user to chose the movement he desires
+    char dir;
+    movement* move;
+    int status = 0; //Will be true if a correct movement is recorded
+    while (!status) {
+        printf("Entrez la coordonnée verticale de la balle à déplacer\n");
+        scanf("%hhd \n", &(move->posix));
+        printf("Entrez la coordonnée horizontale de la balle à déplacer\n");
+        scanf("%hhd \n", &(move->posiy));
 
-    printf("Ce mouvement n'est pas possible. Veuillez en proposer un autre :\n");
+        int ok = 0;
+        while(!ok) {
+            printf("Entrez la direction du mouvement (n,s,e,o) : \n");
+            scanf("%c \n", &dir);
+            if (dir=='n') {
+                ok++;
+                move->dir = north;
+            }
+            else if (dir=='s') {
+                ok++;
+                move->dir = south;
+            }
+            else if (dir=='e') {
+                ok++;
+                move->dir = east;
+            }
+            else if (dir=='o') {
+                ok++;
+                move->dir = west;
+            }
+            else {
+                ok =0;
+                printf("Erreur lors de l'entrée \n");
+            }
+        }
+        status = correctMove(board, move);
+        if (status==0){
+            printf("Ce mouvement n'est pas possible. Veuillez en proposer un autre :\n");
+        } 
+    }
+    doMove(board, move);
+    
 }
 
 void printBoard(state board[7][7]) {
@@ -73,7 +171,7 @@ void printBoard(state board[7][7]) {
     }
 }
 
-void user_game() {
+void userGame() {
     //Define 7x7 board and a counter
     state board[7][7];
     int turn = 0;
@@ -81,11 +179,11 @@ void user_game() {
     //Initialise the board
     initBoard(board);
 
-    while (possible_move(board)){
+    while (possibleMove(board)){
         printf("----------   Début du tour %d ----------\n", turn);
         //Print the board
         printBoard(board);
-        //user_move();
+        userMove(board);
     }
 }
 
@@ -93,26 +191,25 @@ int main(){
     unsigned char status;
 
     //intro
-    printf("WELCOME TO SOLITAIRE VERSION 1.0\n");
+    printf("Bienvenue dans le Solitaire v0.1\n");
     printf("\n");
 
     //mode choice
-    printf("PRESS 1 TO PLAY OR 2 FOR SELF RESOLUTION\n");
-    scanf("%hhd", &status);
+    printf("Appuyez sur '1' pour jouer ou '2' pour une résolution automatique :\n");
+    scanf("%hhd\n", &status);
 
     //checking for input errors
     while (status!=1 && status!=2) {
-        printf("INPUT ERROR !!\n");
-        printf("\n");
-        printf("PRESS 1 TO PLAY OR 2 FOR SELF RESOLUTION\n");
-        scanf("%hhd", &status);
+        printf("Erreur lors de l'entrée. Veuillez réessayer;\n");
+        printf("Appuyez sur '1' pour jouer ou '2' pour une résolution automatique :\n");
+        scanf("%hhd\n", &status);
     }
 
     //actual game
-    user_game();
+    userGame();
 
     //bye bye
     printf("\n");
-    printf("GOOD BYE ^~^\n");
+    printf("Au revoir ^~^\n");
     return 0;
 }
