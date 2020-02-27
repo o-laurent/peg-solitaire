4563102bba02c95ba6d246fefe91a4e74b315e7e
M. Johnny Yammine <johnny.yammine@ensta-paris.fr>
2020-02-27 23:40:20 +0100

    feasible

--- feasible.c
+++ feasible.c
@@ -2,42 +2,116 @@
 
 
 int ruleOftheThree(state** board) {
-    //Returns 0 if the board is not feasible according to the ruleOfTheThree
+    
 
-    /*Use do while
-    variable stop : quand stop = 1, on arrête
-    i
-    j
-    endBoard = board avec une bille seulement en (i,j)
-    do {
-        int square3b[3][3] carré 3*3 qui correspond au board
-        int square2b[2][2] carré 2*2 qui correspond au board
-        int square3e[3][3] carré 3*3 qui correspond au endboard
-        int square2e[2][2] carré 2*2 qui correspond au endboard
-
-        square3b = board2Square3(board)
-        square3b = useEquiv(square3b)
-        square2b = square32Square2(square3b)
-
-        square3e = board2Square3(endBoard)
-        square3e = useEquiv(square3e)
-        square2e = square32Square2(square3e)
-        stop = squareEqual(square2e,square2b)
-    }
-
-    while (!stop and conditions sur i j : on a tout testé)*/
     return 1;
 }
 
-/*
-int** board2Square3(state** board) {
-    //Transforms the board to a 3*3 square with the usual transmations 
-    Au dessus de cube on fait descendre positivement (la case (i,j) perd 1, celle du dessous perd 1 et celle d'encore en dessous gagne 1)
-    A gauche du cube on fait droite positivement (la case (i,j) perd 1, celle de droite perd 1 et celle d'encore de droite gagne 1)
-    A droite du cube on fait gauche positivement (la case (i,j) perd 1, celle de gauche perd 1 et celle d'encore de gauche gagne 1)
-    En dessous du cube on fait monter positivement (la case (i,j) perd 1, celle du dessus perd 1 et celle d'encore en dessus gagne 1)
-}
 
+int** board2Square3(state** board) {
+    int x=4;
+    int y=4;
+
+    int **boardNum;
+    boardNum = malloc(sizeof(*boardNum) * 7);
+    for (int i=0;i<7;i++) {
+       boardNum[i] = malloc(sizeof(**boardNum)*7);
+    }
+
+    for(int i=0;i<7;i++){
+        for(int j=0;j<7;j++){
+            if (board[i][j]==out) boardNum[i][j]=-100000;
+            else if (board[i][j]==empty) boardNum[i][j]=0;
+            else if (board[i][j]==ball) boardNum[i][j]=1;
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<7;j++){
+            if (boardNum[i][j]!=0 && boardNum[i][j]!=-100000){
+                int val=boardNum[i][j];
+                if (val>0){
+                boardNum[i][j]=0;
+                boardNum[i+1][j]=boardNum[i+1][j]-val;
+                boardNum[i+2][j]=boardNum[i+2][j]+val;
+                }
+                if (val<0){
+                boardNum[i][j]=0;
+                boardNum[i+1][j]=boardNum[i+1][j]-val;
+                boardNum[i+2][j]=boardNum[i+2][j]-val;
+                }
+            }
+        }
+    }
+
+    for(int i=6;i>4;i--){
+        for(int j=0;j<7;j++){
+            if (boardNum[i][j]!=0 && boardNum[i][j]!=-100000){
+                int val=boardNum[i][j];
+                if (val>0){
+                boardNum[i][j]=0;
+                boardNum[i-1][j]=boardNum[i-1][j]-val;
+                boardNum[i-2][j]=boardNum[i-2][j]+val;
+                }
+                if (val<0){
+                boardNum[i][j]=0;
+                boardNum[i-1][j]=boardNum[i-1][j]-val;
+                boardNum[i-2][j]=boardNum[i-2][j]-val;
+                }
+            }
+        }
+    }
+
+    for(int i=2;i<5;i++){
+        for(int j=0;j<2;j++){
+            if (boardNum[i][j]!=0 && boardNum[i][j]!=-100000){
+                int val=boardNum[i][j];
+                if (val>0){
+                boardNum[i][j]=0;
+                boardNum[i][j+1]=boardNum[i][j+1]-val;
+                boardNum[i][j+2]=boardNum[i][j+2]+val;
+                }
+                if (val<0){
+                boardNum[i][j]=0;
+                boardNum[i][j+1]=boardNum[i][j+1]-val;
+                boardNum[i][j+2]=boardNum[i][j+2]-val;
+                }
+            }
+        }
+    }
+
+    for(int i=2;i<5;i++){
+        for(int j=6;j>4;j--){
+            if (boardNum[i][j]!=0 && boardNum[i][j]!=-100000){
+                int val=boardNum[i][j];
+                if (val>0){
+                boardNum[i][j]=0;
+                boardNum[i][j-1]=boardNum[i][j-1]-val;
+                boardNum[i][j-2]=boardNum[i][j-2]+val;
+                }
+                if (val<0){
+                boardNum[i][j]=0;
+                boardNum[i][j-1]=boardNum[i][j-1]-val;
+                boardNum[i][j-2]=boardNum[i][j-2]-val;
+                }
+            }
+        }
+    }
+
+    int **square=malloc(sizeof(*square) * 3);
+    for (int i=0;i<3;i++) {
+       square[i] = malloc(sizeof(**square)*3);
+    }
+
+    for(int i=0;i<3;i++){
+        for(intj=0;j<3;j++){
+            square[i][j]=boardNum[i+2][j+2];
+        }
+    }
+    return(square);
+
+}
+/*
 int** useEquiv(int square2[2][2]) {
     // tu appliques d'abord les les 2 dernières règles 
     Puis ensuite les premières dans les 6 directions (3verticales, 3 horizontales) avec des if 
